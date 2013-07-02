c2170d8414367b5c4a5f0e423b5a40ec75556877
Chris Schwartz <chris.f.schwartz@gmail.com>
2013-07-01 22:32:20 -0500

    finish cleanup for strategy pattern

--- strategy-pattern/code/with_classes/run.rb
+++
@@ -1,9 +0,0 @@
-require './Report'
-require './HTMLFormatter'
-require './PlainTextFormatter'
-
-report = Report.new(HTMLFormatter.new)
-report.output_report
-
-report.formatter = PlainTextFormatter.new
-report.output_report
